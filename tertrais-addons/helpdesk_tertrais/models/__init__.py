1f0f1167982b8be77e668b20b7e090efc086ef8f
SEMLALI Loubna <loubna@ouiddoo.com>
2023-08-31 13:01:47 +0200

    [ADD] supp description from helpdesk ticket

--- tertrais-addons/helpdesk_tertrais/models/__init__.py
+++ tertrais-addons/helpdesk_tertrais/models/__init__.py
@@ -1,8 +1,2 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from . import sale_order
-from . import account_move_line
-from . import product_template
-from . import stock_move
-from . import stock_move_line
-from . import stock_production_lot
-from . import mrp_production
+from . import helpdesk
