2327234e005a8737ef1dfabc057318e8c416131b
SEMLALI Loubna <loubna@ouiddoo.com>
2023-10-02 15:34:46 +0200

    [ADD] subscription dev

---
+++ tertrais-addons/sale_subscription_tertrais/__manifest__.py
@@ -0,0 +1,15 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Subscription Tertrais",
+    "version": "16.0.1",
+    'category': 'sale',
+    "summary": "This module allows you to  ***",
+    "author": "Ouiddoo",
+    "license": "AGPL-3",
+    "depends": ["sale_subscription"],
+
+    "data": [
+             "views/sale_order_views.xml",
+             ],
+    "installable": True,
+}
