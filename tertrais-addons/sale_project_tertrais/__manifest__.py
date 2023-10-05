5541e2251afb746988bab67eacc29eba7bb44bc6
SEMLALI Loubna <loubna@ouiddoo.com>
2023-10-05 20:08:27 +0200

    [FIX] sale order bug

---
+++ tertrais-addons/sale_project_tertrais/__manifest__.py
@@ -0,0 +1,14 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Sale Project Tertrais",
+    "version": "16.0.1",
+    'category': 'sale',
+    "summary": "This module allows you to fix odoo sale_project bug***",
+    "author": "Ouiddoo",
+    "license": "AGPL-3",
+    "depends": ["sale_project"],
+
+    "data": [
+             ],
+    "installable": True,
+}
