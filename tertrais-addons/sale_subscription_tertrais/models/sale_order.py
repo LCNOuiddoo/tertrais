125a0c1a0c262478598d612fb0a15cffa55e1735
Louis-de-Bontin <louis2bontin@gmail.com>
2025-12-18 15:04:34 +0100

    Restore code

--- tertrais-addons/sale_subscription_tertrais/models/sale_order.py
+++ tertrais-addons/sale_subscription_tertrais/models/sale_order.py
@@ -8,18 +8,18 @@ _logger = logging.getLogger(__name__)
 class SaleOrder(models.Model):
     _inherit = 'sale.order'
 
-    # def start_subscription(self):
-    #     self.start_date = fields.Date.today()
-    #     self.next_invoice_date = fields.Date.today()
-    #     return True
+    def start_subscription(self):
+        self.start_date = fields.Date.today()
+        self.next_invoice_date = fields.Date.today()
+        return True
 
-    # def _confirm_subscription(self):
-    #     sub_to_skip = []
-    #     for sub in self:
-    #         if sub.start_date:
-    #             sub_to_skip.append(sub.id)
-    #     super()._confirm_subscription()
-    #     for sub in self:
-    #         if sub.id not in sub_to_skip:
-    #             sub.start_date = False
-    #             sub.next_invoice_date = False
+    def _confirm_subscription(self):
+        sub_to_skip = []
+        for sub in self:
+            if sub.start_date:
+                sub_to_skip.append(sub.id)
+        super()._confirm_subscription()
+        for sub in self:
+            if sub.id not in sub_to_skip:
+                sub.start_date = False
+                sub.next_invoice_date = False
